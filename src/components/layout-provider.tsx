0c94b4d742f837a4d3cf4fe0d7990d55e3e91d69
emilie.gelhardt <emilie.gelhardt@gmail.com>
2026-01-10 12:59:43 +0000

    Logo Anzeige am Anfang

---
+++ src/components/layout-provider.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Loader from "@/components/loader";
+import CookieConsent from "@/components/cookie-consent";
+import { Toaster } from "@/components/ui/toaster";
+
+
+export default function LayoutProvider({
+  children,
+}: Readonly<{
+  children: React.React_node;
+}>) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <CookieConsent />
+          {children}
+          <Toaster />
+        </>
+      )}
+    </>
+  );
+}
